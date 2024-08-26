f4ee13bc350232e99e4fab8ae94f24f24372ad92
mrandarabski <asad.and@gmail.com>
2024-08-26 14:50:27 +0200

    Section9

--- gatewayserver/src/main/java/com/lvgod/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/lvgod/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.lvgod.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -9,6 +14,28 @@ public class GatewayserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 		System.out.println("Hi the Gateway Server started to serve other microservices");
+	}
+
+	@Bean
+	public RouteLocator lvgodConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/lvgod/accounts/**")
+						.filters(f -> f.rewritePath("/lvgod/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/lvgod/loans/**")
+						.filters(f -> f.rewritePath("/lvgod/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/lvgod/cards/**")
+						.filters(f -> f.rewritePath("/lvgod/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS"))
+				.build();
+
 	}
 
 }
